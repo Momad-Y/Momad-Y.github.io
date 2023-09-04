5304a3a3815d2509280ac4d37412d7231d019877
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2023-09-04 06:31:57 +0300

    Created the css and js files for the about page

--- static/js/shared.js
+++ static/js/shared.js
@@ -1,26 +1,47 @@
+// Initialing the overlay element and the page number.
 const overlayElement = document.querySelector("#overlay");
 var pageNum = 1;
-var firstLoad = true;
 
+/**
+ * Handles the navigation button click event.
+ * Retrieves the page name and redirect link based on the pressed page number.
+ * Updates the overlay element with the page name, animates the overlay to cover the screen,
+ * and redirects the user to the specified page after a delay.
+ * @param {number} pressedPage - The number of the pressed page button.
+ * @returns {void}
+ */
 function navBtn(pressedPage) {
-    let pressedPageDataArray = pressedMenuData(pressedPage);
-    let pageTitle = pressedPageDataArray[0];
-    let pageLink = pressedPageDataArray[1];
+    // Retrieve the page title and page link based on the pressed page number
+    const [pageTitle, pageLink] = pressedPageData(pressedPage);
 
+    // Set the height of the overlay element to cover the screen
     overlayElement.style.height = "100%";
+
+    // Update the overlay element with the page title
     overlayElement.innerHTML = pageTitle;
 
+    // Set a timeout function to animate the overlay and redirect the user after a delay
     setTimeout(() => {
+        // Animate the overlay by setting its height to 0%
         overlayElement.style.height = "0%";
+
+        // Set another timeout function to redirect the user after the animation completes
         setTimeout(() => {
+            // Redirect the user to the specified page
             window.location.replace(pageLink);
-        }, 1300);
+        }, 1000);
     }, 1300);
 }
 
-function pressedMenuData(pressedPage) {
+/**
+ * Returns the page title and redirect link based on the pressed page number.
+ * @param {number} pressedPage - The pressed page number.
+ * @returns {Array} - An array containing the page title and redirect link.
+ */
+function pressedPageData(pressedPage) {
     let pageName;
     let redirectLink;
+
     switch (pressedPage) {
         case 1:
             pageName = "Home.";
@@ -37,5 +58,6 @@ function pressedMenuData(pressedPage) {
             pageName = "Contact Me.";
             break;
     }
+
     return [pageName, redirectLink];
 }
