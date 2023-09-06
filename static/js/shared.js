7b6c9f6500901859bed26993222c116fa18e6808
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2023-09-06 05:04:25 +0300

    Added functionality for the theme toggler

--- static/js/shared.js
+++ static/js/shared.js
@@ -1,6 +1,41 @@
-// Initialing the overlay element and the page number.
+/**
+ * Initialing the overlay element, the root class of the css file and an array called `themes`
+ * that contains two objects representing different themes.
+ * Each object contains CSS variable-value pairs that define the styling for the corresponding theme the page number.
+ */
 const overlayElement = document.querySelector("#overlay");
-var pageNum = 1;
+const themeToggler = document.querySelector("#toggler-input");
+const rootClass = document.documentElement.style;
+const themes = [
+    {
+        "--main-text-color": "#c8c4bd",
+        "--card-border-color": "#7c7365",
+        "--card-shadow-color": "#26292a",
+        "--subtitle-text-color": "#b8b2a9",
+        "--important-normal-text-color": "#bcb7ae",
+        "--normal-text-color": "#a8a095",
+        "--icons-color": "#c8c4bd",
+        "--card-bg-color": "#313537",
+        "--main-bg-color": "#004d4d",
+        "--sec-bg-color": "#181a1b",
+        "--third-bg-color": "#25282a",
+        "--btn-bg-color": "#0c5bae",
+    },
+    {
+        "--main-text-color": "#323232",
+        "--card-border-color": "#323232",
+        "--card-shadow-color": "#323232",
+        "--subtitle-text-color": "#4c4c4c",
+        "--important-normal-text-color": "#505050",
+        "--normal-text-color": "#666666",
+        "--icons-color": "#323232",
+        "--card-bg-color": "#d3d3d3",
+        "--main-bg-color": "#006060",
+        "--sec-bg-color": "#ffffff",
+        "--third-bg-color": "#e8e8e8",
+        "--btn-bg-color": "#2d8cf0",
+    },
+];
 
 /**
  * Handles the navigation button click event.
@@ -61,3 +96,30 @@ function pressedPageData(pressedPage) {
 
     return [pageName, redirectLink];
 }
+
+/**
+ * Toggles the theme of the web page.
+ * @param {number} requestedTheme - The theme to be applied. 1 represents the light theme and 0 represents the dark theme.
+ * @returns {void}
+ */
+function toggleTheme(requestedTheme) {
+    Object.entries(themes[requestedTheme]).forEach(([property, value]) => {
+        rootClass.setProperty(property, value);
+    });
+}
+
+/**
+ * Adds an event listener to the themeToggler element to listen for changes in its state.
+ * When the state changes, retrieves the checked state of the themeToggler element and
+ * calls the toggleTheme function with the requestedTheme as an argument.
+ *
+ * @param {Event} event - The event object from the change event.
+ * @returns {void}
+ */
+themeToggler.addEventListener("change", (event) => {
+    // Declare and initialize the requestedTheme variable based on the checked state of the themeToggler element
+    let requestedTheme = event.target.checked ? 0 : 1;
+
+    // Call the toggleTheme function with the requestedTheme as an argument
+    toggleTheme(requestedTheme);
+});
